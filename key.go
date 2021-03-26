80c3941147124792a5ed37d9970eb8951fcd2cd4
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-03-26 15:51:37 +0800

    feat: add KeyType.Available method

--- key.go
+++ key.go
@@ -10,24 +10,33 @@ import (
 )
 
 // keyIsValid returns true if the key is valid for flatfs.
-// Allows keys that match [0-9A-Z+-_=].
+
 func keyIsValid(k key.Key) bool {
-	ks := k.String()
-	if len(ks) < 2 || ks[0] != '/' {
+	switch k.KeyType() {
+	// Allows StrKeys that match [0-9A-Z+-_=].
+	case key.KeyTypeString:
+		ks := k.String()
+		if len(ks) < 2 || ks[0] != '/' {
+			return false
+		}
+		for _, b := range ks[1:] {
+			if '0' <= b && b <= '9' {
+				continue
+			}
+			if 'A' <= b && b <= 'Z' {
+				continue
+			}
+			switch b {
+			case '+', '-', '_', '=':
+				continue
+			}
+			return false
+		}
+		return true
+	// Allows all BytesKeys, will be base64url encoded
+	case key.KeyTypeBytes:
+		return true
+	default:
 		return false
 	}
-	for _, b := range ks[1:] {
-		if '0' <= b && b <= '9' {
-			continue
-		}
-		if 'A' <= b && b <= 'Z' {
-			continue
-		}
-		switch b {
-		case '+', '-', '_', '=':
-			continue
-		}
-		return false
-	}
-	return true
 }
